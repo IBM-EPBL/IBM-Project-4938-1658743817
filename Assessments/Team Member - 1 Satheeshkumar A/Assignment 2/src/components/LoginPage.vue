25d0fcd8329242074024575b73aaca5ec6e12c7a
Satheesh01 <satheeshkumar.a2019ece@sece.ac.in>
2022-10-18 22:39:45 +0530

    Assignment submitted

---
+++ Assessments/Team Member - 1 Satheeshkumar A/Assignment 2/src/components/LoginPage.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <base-header></base-header>
+    <v-main>
+      <v-card elevation="2" class="loginCard">
+        <v-form @submit.prevent="">
+          <v-banner><h3>LOGIN</h3></v-banner>
+          <v-row>
+            <v-col>
+              <v-row>
+                <v-col>
+                  <v-text-field
+                    label="Email Id"
+                    type="email"
+                    v-model="inputName"
+                  >
+                  </v-text-field
+                ></v-col>
+              </v-row>
+              <v-row>
+                <v-col
+                  ><v-text-field
+                    :append-icon="passwordVisible ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required]"
+                    :type="passwordVisible ? 'text' : 'password'"
+                    name="input-10-2"
+                    v-model="inputPassword"
+                    label="Password"
+                    hint="At least 8 characters"
+                    class="input-group--focused"
+                    @click:append="passwordVisible = !passwordVisible"
+                  >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col class="">
+              <base-button><h3>LOGIN</h3></base-button></v-col
+            ><v-col
+              ><router-link to="/signup" right>
+                <base-button class="signup-btn"
+                  ><h3>SIGNUP</h3></base-button
+                ></router-link
+              ></v-col
+            ></v-row
+          >
+          <v-row class="text-center">
+            <router-link to="/forgot">
+              <p>did you forgot password?</p>
+            </router-link>
+          </v-row>
+        </v-form>
+      </v-card>
+    </v-main>
+    <base-footer></base-footer>
+  </div>
+</template>
+
+<script>
+import BaseFooter from "@/UI/BaseFooter.vue";
+export default {
+  components: { BaseFooter },
+  data() {
+    return {
+      inputName: "",
+      inputPassword: "",
+      passwordVisible: false,
+      rules: {
+        required: (value) => !!value || "Required.",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.appBar {
+  display: flex;
+  align-content: space-between;
+}
+
+a {
+  text-decoration: none;
+}
+.loginCard {
+  margin: 10rem auto;
+  max-width: 45rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ Assessments/Team Member - 1 Satheeshkumar A/Assignment 2/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    redirect: "login",
+  },
+  {
+    name: "LogIn",
+    path: "/login",
+    component: () => import("../components/LoginPage"),
+  },
+  {
+    name: "SignUp",
+    path: "/signup",
+    component: () => import("../components/SignupPage"),
+  },
+  {
+    name: "ForgotPassword",
+    path: "/forgot",
+    component: () => import("../components/ForgotPassword.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
